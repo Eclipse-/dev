f7f8185b2010c60085a17ddc42f71d3bb7c759dc
Eclipse <eclipse@tukui.org>
2013-02-05 15:59:36 +0000

    Buff update.

--- dev/modules/buffs/header.lua
+++ dev/modules/buffs/header.lua
@@ -1,10 +1,50 @@
 local T, C, L, G = unpack(Tukui)
 
--- Modify Auras
+-- auras
 do
-	G.Auras.Buffs:ClearAllPoints()
-	G.Auras.Buffs:SetPoint("TOPRIGHT", G.Maps.Minimap, "TOPLEFT", -5, 0)
-	
-	-- G.Auras.Debuffs:ClearAllPoints()
-	-- G.Auras.Debuffs:SetPoint()
+	local frame = CreateFrame("Frame", "TukuiAuras")
+	frame.content = {}
+
+	local frames = {
+		TukuiAurasPlayerBuffs,
+		TukuiAurasPlayerDebuffs,
+		TukuiAurasPlayerConsolidate,
+	}
+
+	local frameUpdate = function(self)
+		for i = 1, self:GetNumChildren() do
+			local child = select(i, self:GetChildren())
+			local duration = child.Duration
+			local count = child.Count
+			
+			-- create shadows
+			if not child.shadow then
+				child:CreateShadow("Default")
+			end
+			
+			-- duration text
+			if duration then
+				duration:ClearAllPoints()
+				duration:Point("TOP", child, "BOTTOM", 0, -2)
+				duration:SetFont(C.media.caith, 12, "OUTLINE")
+			end
+			
+			-- stack text
+			if count then
+				count:ClearAllPoints()
+				count:Point("BOTTOMRIGHT", -2, 4)
+				count:SetFont(C.media.caith, 12, "OUTLINE")
+			end
+		end
+	end
+
+	for _, frame in pairs(frames) do
+		frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+		frame:HookScript("OnAttributeChanged", frameUpdate)
+		frame:HookScript("OnEvent", frameUpdate)
+	end
+
+	TukuiAurasPlayerBuffs:ClearAllPoints()
+	TukuiAurasPlayerBuffs:Point("TOPRIGHT", G.Maps.Minimap, "TOPLEFT", -5, 0)
+	TukuiAurasPlayerBuffs:Size(25) -- need config
 end
