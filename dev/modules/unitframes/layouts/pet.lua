0d47ae1ecf2b989efbcd391b8ded05228a622198
Jonas Holmström <jholmstroem@gmail.com>
2013-02-08 21:11:08 +0100

    Pet unitframe

--- dev/modules/unitframes/layouts/pet.lua
+++ dev/modules/unitframes/layouts/pet.lua
@@ -1,45 +1,29 @@
--- local T, C, L, G = unpack(Tukui) 
+local T, C, L, G = unpack(Tukui) 
 
--- local player = G.UnitFrames.Player
+local pet = G.UnitFrames.Pet
+local adjustXY = 0
 
--- do
--- end
+do
+	-- kill
+	pet.panel:Kill()
+	pet.shadow:Kill()
+	pet:SetBackdrop(nil)
+	
+	-- setup
+	T.UnitframePanels(pet, "pet")
+	
+	-- size
+	pet:Size(129, 36)
+	
+	-- position
+	pet:ClearAllPoints()
+	pet:Point("TOPLEFT", G.UnitFrames.Player, "BOTTOMLEFT", 0, -9+adjustXY)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	-- health bar
+	pet.Health:Height(18)
+	
+	-- targettarget text
+	pet.Name:ClearAllPoints()
+	pet.Name:Point("CENTER", pet.Panel, "CENTER", 0, 0) 
+	pet.Name:SetFont(C.media.caith, 12)
+end
